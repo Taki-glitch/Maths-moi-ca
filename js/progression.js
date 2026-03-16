8df2b7039e3abbf7b2a9d5997cf4d96faad9fb22
Taki-glitch <tsqdevin@gmail.com>
2026-03-16 12:10:28 +0100

    Harmonise les formules LaTeX dans tous les chapitres

--- js/progression.js
+++ js/progression.js
@@ -9,3 +9,23 @@ function enregistrerScore(chapitre, score, total) {
 
   localStorage.setItem("progressionMaths", JSON.stringify(progression));
 }
+
+(function activerRenduMaths() {
+  if (window.MathJax) return;
+
+  window.MathJax = {
+    tex: {
+      inlineMath: [['$', '$'], ['\\(', '\\)']],
+      displayMath: [['$$', '$$'], ['\\[', '\\]']]
+    },
+    options: {
+      skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code']
+    }
+  };
+
+  const script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
+  script.async = true;
+  script.id = 'mathjax-script';
+  document.head.appendChild(script);
+})();
